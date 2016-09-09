5ec7c66e2ae95f640c5550e67a4e23f5500307e4
Federico Dionisi <dionisi.federico@gmail.com>
2016-09-09 16:27:22 +0200

    update navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,17 @@ import Navs from './Navs'
 export default {
     name: 'navbar',
     props: {
+        bg: {
+            type: String,
+            default: 'faded'
+        },
+        luminosity: {
+            type: String,
+            default: 'light'
+        },
+        fullWidth: Boolean,
+        fixedTop: Boolean,
+        fixedBottom: Boolean,
         brand: {
             type: Object,
             default: () => ({})
@@ -16,13 +27,20 @@ export default {
     },
     render(h) {
         const className = {
-            navbar: true
+            navbar: true,
+            'navbar-full': this.fullWidth,
+            'navbar-fixed-top': this.fixedTop,
+            'navbar-fixed-bottom': !this.fixedTop && this.fixedBottom,
+            [`navbar-${this.luminosity}`]: true,
+            [`bg-${this.bg}`]: true
         }
 
         return <nav class={className}>
             { this.brand.text && <a class="navbar-brand" on-click={emitEvent('brand-click', this)} href={this.brand.href || '#'}>{ this.brand.text }</a> }
 
             { this.list.length && <Navs class="navbar-nav" list={this.list}/> }
+
+            { this.$slots.default }
         </nav>
     }
 }
